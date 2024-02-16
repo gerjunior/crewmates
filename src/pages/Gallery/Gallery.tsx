1b23fd96ca44afb093433b45cd204c7b1727d272
Geraldo Silva <geraldojuni@hotmail.com>
2024-02-16 20:45:50 -0300

    feat: empty crewmate gallery

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -1,3 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export const Gallery = () => {
-  return <div>Gallery</div>;
+  return (
+    <div className='p-10 flex flex-col justify-center items-center gap-10 h-screen'>
+      <h2 className='text-5xl font-bold'>Your Crewmate Gallery!</h2>
+      <p className='text-xl'>You haven't made a crewmate yet!</p>
+      <Link to='/create'>
+        <button>Create one here!</button>
+      </Link>
+    </div>
+  );
 };
