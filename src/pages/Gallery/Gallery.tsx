689346c033e464d49b7128e9ebb4b0f4a96a887f
Geraldo Silva <geraldojuni@hotmail.com>
2024-02-17 18:20:29 -0300

    feat: add number of crewmates and avg speed to gallery

--- src/pages/Gallery/Gallery.tsx
+++ src/pages/Gallery/Gallery.tsx
@@ -3,12 +3,39 @@ import useSWR from 'swr';
 import { Card } from './Card/Card';
 import { getCrewmates } from '../../client';
 
+const MIN_CREWMATES_TO_SHIP = 10;
+
 export const Gallery = () => {
   const { data: crewmates } = useSWR('crewmates', getCrewmates);
+  const averageSpeed = crewmates?.length
+    ? Math.round(
+        crewmates?.reduce(
+          (acc, crewmate) => acc + parseInt(crewmate.speed),
+          0,
+        ) / crewmates.length,
+      )
+    : 0;
 
   return (
     <div className='p-10 flex flex-col justify-center items-center gap-10 h-screen'>
       <h2 className='text-5xl font-bold'>Your Crewmate Gallery!</h2>
+      {crewmates?.length && crewmates?.length >= MIN_CREWMATES_TO_SHIP && (
+        <div>
+          <p className='text-xl'>
+            You have{' '}
+            <span className='text-2xl font-bold text-blue-500'>
+              {crewmates?.length}
+            </span>{' '}
+            crewmates! You're ready to ship!
+          </p>
+          <p className='text-xl'>
+            The average speed of your crew is{' '}
+            <span className='text-2xl font-bold text-blue-500'>
+              {averageSpeed} mph
+            </span>
+          </p>
+        </div>
+      )}
       {!crewmates?.length && (
         <>
           <p className='text-xl'>You haven't made a crewmate yet!</p>
